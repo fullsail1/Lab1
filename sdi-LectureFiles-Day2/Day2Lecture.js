af1b0105b381d97b27fc907e63dece1a06ab9b71
Anthony Hicks <osahon10@fullsail.edu>
2014-10-30 19:39:55 -0400

    well again

---
+++ sdi-LectureFiles-Day2/Day2Lecture.js
@@ -0,0 +1,20 @@
+alert("JavaScript works! bossplaya on deck sdi 1409 day 2 lecture DC DMV WE get it poppin");
+
+var myHomeAddress = "187 Hollow point driveby";
+var Favorite Clu = "Versace store ";
+var NumberOfMiles = 7.18;
+var typeOfCarToDrive = "benz";
+
+//my confirms
+myHomeAddress = prompt("reconfirm your address:", " 187 Hollow point driveby");
+typeOfCarToDrive = confirm(" do you want to drive your benz?");
+
+
+
+
+//my outputs
+console.log("I run this city but where i really chill, really get it in at is" + myHomeAddress+ ".");
+console.log("I cop all the hot newnew at the " + myFavoriteStore + ".");
+console.log(" Your milage is " + NumberOfMiles + "to your destination" + ".");
+console.log(" Why you bring your " + typeOfCarToDrive + " out to stunt on the parking lot" + ".");
+
---
+++ sdi-LectureFiles-Day2/lecture3.js
@@ -0,0 +1,25 @@
+//alert("JavaScript works! bossplaya on deck sdi 1409 day 2 lecture DC DMV WE get it poppin");
+
+var myHomeAddress = "123 Fire Lane";
+var myFavoriteStore = " Versace store ";
+var NumberOfMiles = 7.18;
+var Goodweather = false;
+
+
+
+//my outputs
+console.log("I invite my friends to " + myHomeAddress+ " on football sundays.");
+console.log(" My boss buys suits from " + myFavoriteStore + ".");
+console.log(" Your milage is " + NumberOfMiles + "to your destination" + ".");
+console.log(" Is it good grillin' weather " + Goodweather + "?");
+
+//my confirms
+myHomeAddress = prompt("reconfirm your address:", " 123 Fire Lane ");
+Goodweather = confirm(" Did the weather get better? " );
+
+// output 2
+
+console.log(" We'll still meet at " + myHomeAddress + ".");
+console.log( Goodweather + " The weather got better" + " So bring some charcoal!");
+
+
